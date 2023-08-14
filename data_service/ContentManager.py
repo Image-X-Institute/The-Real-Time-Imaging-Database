a9c274a336d6eaaf7aa8915915e6d48fe7b8d886
Yu Liang <y.liang@sydney.edu.au>
2023-08-14 11:52:23 +1000

    fix bugs

--- data_service/ContentManager.py
+++ data_service/ContentManager.py
@@ -324,8 +324,7 @@ class ContentManager:
                 fractionName = re.search(r'\/([^\/]+)\/$', formatedPath).group(1)
             if fractionName=="":
                 fractionName = fractionNumber
-            formatedPath = re.replace(r'^([^\/]+)\/', "", formatedPath)
-            formatedPath = formatedPath[1:]
+            formatedPath = re.sub(r'^([^\/]+)\/', "", formatedPath)
             relativeFolderPath =  fileTypeToPathMapping[metadata["file_type"]].format(
                             clinical_trial=metadata['clinical_trial'],
                             test_centre=metadata["test_centre"],
