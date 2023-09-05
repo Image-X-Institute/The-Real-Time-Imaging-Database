01df1708dbad6a3bbcb04de2b46074f2d4f58385
Yu Liang <y.liang@sydney.edu.au>
2023-09-05 13:51:25 +1000

    add admin console service

--- scripts/service/admin_service.sh
+++ scripts/service/admin_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script assumes that the current dicrectory is set by the calling unit
-cd /home/learndb/git_repos/The-Real-Time-Imaging-Database/src/data_service
+cd /home/learndb/git_repos/The-Real-Time-Imaging-Database/src/admin_console
 source /home/learndb/.bashrc
 conda activate data_service_env
 nohup python application.py &
