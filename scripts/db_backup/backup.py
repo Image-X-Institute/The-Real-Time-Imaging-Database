76c0d30c28e152533499c5d6a6ac045e1128d912
Yu Liang <y.liang@sydney.edu.au>
2024-03-19 16:14:00 +1100

    fix bugs

--- scripts/db_backup/backup.py
+++ scripts/db_backup/backup.py
@@ -13,19 +13,22 @@ def backup_db():
   imagingdb = config.DB_NAME
 
   # Create backup folder if not exists
+  print("Creating backup folder")
   subprocess.run(f'sudo mkdir -p /data/disk1/DB_BACKUP/{now}', shell=True)
   subprocess.run(f'sudo chown -R learndb:learndb /data/disk1/DB_BACKUP/{now}', shell=True)
 
   # Backup auth database first  
+  print("Backing up auth database")
   subprocess.run(f'pg_dump postgres://{dbuser}:{password}@localhost:5432/{authdb} -Fc > /data/disk1/DB_BACKUP/{now}/{now}_auth_db.dump', shell=True)
   
   # Backup imaging database
+  print("Backing up imaging database")
   subprocess.run(f'pg_dump postgres://{dbuser}:{password}@localhost:5432/{imagingdb} -Fc > /data/disk1/DB_BACKUP/{now}/{now}_imaging_db.dump', shell=True)
 
   print("Backup completed")
 
   if os.path.isdir("/data/rds/PRJ-RPL/2RESEARCH/1_ClinicalData/DB_BACKUP"):
-    subprocess.run(f'cp -r /data/disk1/DB_BACKUP/{now} /data/rds/PRJ-RPL/2RESEARCH/1_ClinicalData/DB_BACKUP')
+    subprocess.run(f'cp -r /data/disk1/DB_BACKUP/{now} /data/rds/PRJ-RPL/2RESEARCH/1_ClinicalData/DB_BACKUP', shell=True)
     print("Backup copied to RDS")
     
 if __name__ == "__main__":
