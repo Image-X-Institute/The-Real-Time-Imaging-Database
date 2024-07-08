c91b6a7df41413a9ff3af2092260a39e5308040a
Yu Liang <70998894+Gahhh@users.noreply.github.com>
2024-07-08 16:54:41 +1000

    Update requirements.txt
    update script
    Update data_service.sh

--- src/db_updater/read.py
+++ src/db_updater/read.py
@@ -2,40 +2,51 @@
 import json
 import os
 import re
-folder_path = "/Volumes/research-data/PRJ-RPL/2RESEARCH/1_ClinicalData/CHIRP/Blacktown/Patient Images"
+import time
+folder_path = "/Volumes/research-data/PRJ-RPL/2RESEARCH/1_ClinicalData/LARK/Westmead/Patient Images"
 
-relist = []
+id_centre_no = {
+
+}
+
 patList = []
 for name in os.listdir(folder_path):
   if not name.find("PAT"):
     patList.append(name)
+    
 
 patList.sort()
+patInfo =[]
 for pat in patList:
   try:
-    fractfolder = os.listdir(f"{folder_path}/{pat}/kV Images")
+    fractfolder = os.listdir(f"{folder_path}/{pat}")
   except FileNotFoundError:
     continue
   fractfolderList = []
   for name in fractfolder:
     if re.fullmatch(r"Fx\d+", name):
+      subfolder = os.listdir(f"{folder_path}/{pat}/{name}")
+      subFractionName = []
+      for sub in subfolder:
+        if re.fullmatch(r"Fx\d+[a-z]", sub) or re.fullmatch(r"Fx\d+", sub):
+          subFractionName.append(sub)
+      if subFractionName == []:
+        subFractionName = [name]
       fracPack = {
         "fraction_number": int(name[2:]),
-        "fraction_name": [name],
+        "fraction_name": subFractionName,
         "fraction_date": ""
       }
       fractfolderList.append(fracPack)
   patPack = {
-    "patient_trial_id": f"CHIRP_{pat[3:]}",
+    "patient_trial_id": id_centre_no[pat],
     "centre_patient_no": int(pat[3:]),
-    "age": 0,
-    "gender": "",
-    "tumour_site": "",
-    "number_of_markers": 0,
-    "LINAC_type": "",
+    "LINAC_type": "Varian Medical Systems ARIA RadOnc",
     "fractions": fractfolderList,
   }
-  relist.append(patPack)
+  patInfo.append(patPack)
 
-with open("data.json", "w") as f:
-  json.dump(relist, f, indent=2)
+with open("lark_new_data.json", "w") as f:
+  json.dump(patInfo, f, indent=2)
+
+
