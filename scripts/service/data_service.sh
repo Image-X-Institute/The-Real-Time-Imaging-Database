3bd0e7c51a26b53a383c9618d9b8e304e5f3f62c
Yu Liang <70998894+Gahhh@users.noreply.github.com>
2024-07-08 16:54:41 +1000

    Update requirements.txt
    update script
    Update data_service.sh

--- scripts/service/data_service.sh
+++ scripts/service/data_service.sh
@@ -2,6 +2,6 @@
 
 # This script assumes that the current dicrectory is set by the calling unit
 cd /home/learndb/git_repos/The-Real-Time-Imaging-Database/src/data_service
-source /home/learndb/.bashrc
-conda activate data_service_env
+# source /home/learndb/.bashrc
+# conda activate data_service_env
 nohup python dataApp.py &
